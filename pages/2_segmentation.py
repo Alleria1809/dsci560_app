34b6a3bdeacc9e4e17bbf4d4384ece73f26881bf
Xiaoyi Gu <alleria@Xiaoyis-MacBook-Pro.local>
2023-03-13 15:56:58 -0700

    update

--- pages/2_segmentation.py
+++ pages/2_segmentation.py
@@ -24,19 +24,19 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
 
-start_year_to_filter, end_year_to_filter = st.select_slider(
-        'Select a range of year',
-        options=[x for x in range(2017, 2023)],
-        value=(2018, 2020))
-st.write('You selected year between', start_year_to_filter, 'and', end_year_to_filter)
+# start_year_to_filter, end_year_to_filter = st.select_slider(
+#         'Select a range of year',
+#         options=[x for x in range(2017, 2023)],
+#         value=(2018, 2020))
+# st.write('You selected year between', start_year_to_filter, 'and', end_year_to_filter)
 
-options = st.multiselect(
-    'Please select the restaurant category:',
-    ['Brunch', 'Japanese', 'American', 'Chinese'],)
+# options = st.multiselect(
+#     'Please select the restaurant category:',
+#     ['Brunch', 'Japanese', 'American', 'Chinese'],)
 
-options_risk = st.multiselect(
-    'Please select the risk level:',
-    ['Low', 'Medium', 'High'],)
+# options_risk = st.multiselect(
+#     'Please select the risk level:',
+#     ['Low', 'Medium', 'High'],)
 
 # st.write('You selected:', options)
 
@@ -77,13 +77,14 @@ def data():
 X = data()
 
 cluster_slider = st.slider(
-    min_value=1, max_value=6, value=2, label="Number of clusters: "
+    min_value=1, max_value=6, value=3, label="Number of clusters: "
 )
 kmeans = KMeans(n_clusters=cluster_slider, random_state=0).fit(X)
 labels = kmeans.labels_
 
-selectbox = st.selectbox("Visualize confidence bounds", [False, True])
+# selectbox = st.selectbox("Visualize confidence bounds", [False, True])
 # stdbox = st.selectbox("Number of standard deviations: ", [1, 2, 3])
+selectbox = False
 
 clrs = ["red", "seagreen", "orange", "blue", "yellow", "purple"]
 
@@ -92,9 +93,9 @@ n_labels = len(set(labels))
 individual = st.selectbox("Individual subplots?", [False, True])
 
 if individual:
-    fig, ax = plt.subplots(ncols=n_labels, figsize=(10, 8))
+    fig, ax = plt.subplots(ncols=n_labels, figsize=(5, 3))
 else:
-    fig, ax = plt.subplots(figsize=(10, 8))
+    fig, ax = plt.subplots(figsize=(7, 4))
 
 for i, yi in enumerate(set(labels)):
     if not individual:
@@ -118,12 +119,13 @@ for i, yi in enumerate(set(labels)):
             # n_std=stdbox,
         )
 
-# plt.tight_layout()
-plt.figure(figsize=(10, 6))
+plt.tight_layout()
+plt.legend([f"cluster {i}" for i in range(cluster_slider)])
+plt.figure(figsize=(5, 3))
 st.write(fig)
 #st.plotly_chart(fig)
 
-res_features = ['Noodles', 'Near USC']
+res_features = ['Noodles', 'Near USC', 'Low Price']
 st.markdown(f'#### common features of these restaurants: ')
-for feature in res_features:
-    st.write(f'{feature}')
+for idx, feature in enumerate(res_features):
+    st.write(f'cluster {idx} : {feature}')
